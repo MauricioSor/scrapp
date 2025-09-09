8a6b61f1b1d0891d416bc00e251231d1dabd212b
Mauricio Soria <mauricioutn2017@gmail.com>
2025-09-08 23:35:30 -0300

    new bucle

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { chromium } from "playwright";
-import { Dialog } from "puppeteer";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
@@ -8,21 +7,19 @@ async function obtenerNovedades() {
     const browser = await chromium.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto("https://consultaexpedientes.justucuman.gov.ar/", { waitUntil: 'load' });
-    // Click en "CAPITAL"
+
     const capitalBtn = page.getByRole('button', { name: /CAPITAL/i });
     await capitalBtn.click();
     console.log("Clickeado CAPITAL");
 
-    // Click en "Documentos y Locaciones"
     const docLocBtn = page.getByRole('button', { name: /Documentos y Locaciones/i });
     await docLocBtn.click();
     console.log("Clickeado Documentos y Locaciones");
 
-    // Escribir en input
     await page.fill('input[name="number"]', "6129/24");
     console.log("Escrito número 6129/24");
-    await page.waitForTimeout(1000); // espera 1 segundo
-    // Click en "Buscar"
+    await page.waitForTimeout(1000);
+
     const buscarBtn = page.locator('button[type="submit"]');
     await buscarBtn.click();
     console.log("Clickeado Buscar");
@@ -32,46 +29,29 @@ async function obtenerNovedades() {
     await botonFila.click();
     console.log("Clickeado botón de la fila específica");
     await page.waitForTimeout(5000);
-    // --- Revisar fechas ---
+
     const celdas = await page.locator('td.align-middle.text-center').allTextContents();
     const fechaHoy = hoyFormatoDDMMYYYY();
-    const hayFechaHoy = celdas.some(fecha => fecha.trim() === fechaHoy);
-    console.log("buscando novedades...");
-
-    // Filtrar solo las celdas que coinciden con la fecha de hoy
     const celdasHoy = celdas.filter(fecha => fecha.trim() === fechaHoy);
-
-    // Saber cuántas hay
     console.log(`Cantidad de novedades de hoy: ${celdasHoy.length}`);
 
     if (celdasHoy.length > 0) {
         console.log("Hay novedades hoy! ", celdasHoy);
-        // aquí podés hacer lo que quieras con esas celdas
-    } else {
-        console.log("No hay novedades hoy");
-    }
-
-    if (hayFechaHoy) {
-        console.log(`✅ Hay expediente nuevo con fecha: ${fechaHoy}`);
-
-        // Enviar correo
         await enviarCorreo(
             `Nuevo expediente: ${fechaHoy}`,
             `Se ha detectado un nuevo expediente con fecha ${fechaHoy}. Revisa el sistema.`
         );
     } else {
-        console.log(`❌ No hay expedientes nuevos para hoy (${fechaHoy})`);
+        console.log("No hay novedades hoy");
         await enviarCorreo(
             `No hay nuevos expedientes: ${fechaHoy}`,
             `No se han detectado nuevos expedientes con fecha ${fechaHoy}.`
         );
-    };
-            await browser.close();  
-        console.log("Navegador cerrado ✅");
-        process.exit(0); // termina el script
-}
+    }
 
-obtenerNovedades();
+    await browser.close();
+    console.log("Navegador cerrado ✅");
+}
 
 function hoyFormatoDDMMYYYY() {
     const hoy = new Date();
@@ -85,17 +65,13 @@ async function enviarCorreo(asunto, mensaje) {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 587,
-        secure: false, // true solo si usas 465
-
+        secure: false,
         auth: {
             user: process.env.MY_MAIL,
             pass: process.env.MY_PASS
         },
-        tls: {
-            rejectUnauthorized: false
-        }
+        tls: { rejectUnauthorized: false }
     });
-
 
     await transporter.sendMail({
         from: '"Alerta Expedientes" <mauBot98@gmail.com>',
@@ -108,7 +84,7 @@ async function enviarCorreo(asunto, mensaje) {
 }
 
 // -----------------------------
-// Bucle con timer cada 2 minutos
+// Bucle con timer cada 1 minutos
 // -----------------------------
 async function loopScraper() {
     while (true) {
@@ -118,10 +94,9 @@ async function loopScraper() {
         } catch (err) {
             console.error("Error ejecutando scraper:", err);
         }
-        console.log("Esperando 1 minutos...");
+        console.log("Esperando 2 minutos...");
         await new Promise(res => setTimeout(res, 1 * 60 * 1000)); // 2 minutos
     }
 }
 
 loopScraper();
-
