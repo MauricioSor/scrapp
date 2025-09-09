29d278bba385a266bc991be9360fd1d72c415814
Mauricio Soria <mauricioutn2017@gmail.com>
2025-09-08 21:30:33 -0300

    v 1.0

---
+++ index.js
@@ -0,0 +1,93 @@
+import { chromium } from "playwright";
+import { Dialog } from "puppeteer";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+async function obtenerNovedades() {
+    const browser = await chromium.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://consultaexpedientes.justucuman.gov.ar/", { waitUntil: 'load' });
+    // Click en "CAPITAL"
+    const capitalBtn = page.getByRole('button', { name: /CAPITAL/i });
+    await capitalBtn.click();
+    console.log("Clickeado CAPITAL");
+
+    // Click en "Documentos y Locaciones"
+    const docLocBtn = page.getByRole('button', { name: /Documentos y Locaciones/i });
+    await docLocBtn.click();
+    console.log("Clickeado Documentos y Locaciones");
+
+    // Escribir en input
+    await page.fill('input[name="number"]', "6129/24");
+    console.log("Escrito número 6129/24");
+    await page.waitForTimeout(1000); // espera 1 segundo
+    // Click en "Buscar"
+    const buscarBtn = page.locator('button[type="submit"]');
+    await buscarBtn.click();
+    console.log("Clickeado Buscar");
+
+    const fila = page.getByRole('row', { name: '6129/24 GARCIA BEATRIZ ANALIA' });
+    const botonFila = fila.getByRole('button');
+    await botonFila.click();
+    console.log("Clickeado botón de la fila específica");
+    await page.waitForTimeout(5000);
+    // --- Revisar fechas ---
+    const celdas = await page.locator('td.align-middle.text-center').allTextContents();
+    const fechaHoy = hoyFormatoDDMMYYYY();
+    const hayFechaHoy = celdas.some(fecha => fecha.trim() === fechaHoy);
+    console.log("buscando novedades...");
+    if (hayFechaHoy) {
+        console.log(`✅ Hay expediente nuevo con fecha: ${fechaHoy}`);
+
+        // Enviar correo
+        await enviarCorreo(
+            `Nuevo expediente: ${fechaHoy}`,
+            `Se ha detectado un nuevo expediente con fecha ${fechaHoy}. Revisa el sistema.`
+        );
+    } else {
+        console.log(`❌ No hay expedientes nuevos para hoy (${fechaHoy})`);
+        await enviarCorreo(
+            `No hay nuevos expedientes: ${fechaHoy}`,
+            `No se han detectado nuevos expedientes con fecha ${fechaHoy}.`
+        );
+    };
+}
+
+obtenerNovedades();
+
+function hoyFormatoDDMMYYYY() {
+    const hoy = new Date();
+    const dd = String(hoy.getDate()).padStart(2, '0');
+    const mm = String(hoy.getMonth() + 1).padStart(2, '0');
+    const yyyy = hoy.getFullYear();
+    return `${dd}/${mm}/${yyyy}`;
+}
+
+async function enviarCorreo(asunto, mensaje) {
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true solo si usas 465
+    
+    auth: {
+        user: process.env.MY_MAIL,
+        pass: process.env.MY_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+
+    await transporter.sendMail({
+        from: '"Alerta Expedientes" <mauBot98@gmail.com>',
+        to: "mauricioutn2017@gmail.com",
+        subject: asunto,
+        text: mensaje
+    });
+
+    console.log("Correo enviado ✅");
+}
+
+
